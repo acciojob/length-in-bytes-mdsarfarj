984369dbab42c011e4ba463a7d3d354abeebebb4
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-11-21 21:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const byteSize = (str) => {
   // write your code here
+	 if (!str || str.length === 0) {
+        return 0;
+    }
+
+    // Create a Blob object with the given string
+    const blob = new Blob([str]);
+
+    // Return the size of the  Blob object in bytes
+    return blob.size;
 };
 
 // Do not change the code below
